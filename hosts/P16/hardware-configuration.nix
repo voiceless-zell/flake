df5623b4a8813cbc7bb3928c60f060a418394d30
voiceless-zell <peter.bouchard2893@proton.me>
2024-07-30 18:14:35 -0400

    reinstall os update hardwareconf

--- hosts/P16/hardware-configuration.nix
+++ hosts/P16/hardware-configuration.nix
@@ -8,19 +8,20 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "thunderbolt" "usbhid" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "thunderbolt" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/55f2ec1e-ea4a-4ed6-980f-9e6b4d8550f6";
+    { device = "/dev/disk/by-uuid/aeaca9f9-6b18-41e6-9152-2ea13c58ee67";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/DF4E-8F84";
+    { device = "/dev/disk/by-uuid/B510-8BB5";
       fsType = "vfat";
+      options = [ "fmask=0022" "dmask=0022" ];
     };
 
   swapDevices = [ ];
