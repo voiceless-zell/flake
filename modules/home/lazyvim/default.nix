e922866aa214f7ce99d8bc29b7d1d6e704afae48
voiceless-zell <peter.bouchard2893@proton.me>
2024-08-06 17:27:19 -0400

    fix lazyvim

--- modules/home/lazyvim/default.nix
+++ modules/home/lazyvim/default.nix
@@ -110,6 +110,12 @@ in {
           spec = {
             { "LazyVim/LazyVim", import = "lazyvim.plugins" },
             -- The following configs are needed for fixing lazyvim on nix
+            
+          { "nvim-treesitter/nvim-treesitter",
+             opts = function(_, opts)
+              opts.ensure_installed = {}
+            end,
+          },
             -- force enable telescope-fzf-native.nvim
             { "nvim-telescope/telescope-fzf-native.nvim", enabled = true },
             -- disable mason.nvim, use programs.neovim.extraPackages
@@ -118,7 +124,6 @@ in {
             -- import/override with your plugins
             { import = "plugins" },
             -- treesitter handled by xdg.configFile."nvim/parser", put this line at the end of spec to clear ensure_installed
-            { "nvim-treesitter/nvim-treesitter", opts = { ensure_installed = {} } },
             { "epwalsh/obsidian.nvim", version = "*",  ft= "markdown", lazy = false },
             { "vimwiki/vimwiki", version = "*"}
           },
