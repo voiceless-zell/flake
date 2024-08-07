08b57048409f929d5431fe7ae177559f26a23208
voiceless-zell <peter.bouchard2893@proton.me>
2024-08-06 20:14:43 -0400

    SC working monitor adjustment

--- hosts/P16/nix.nix
+++ hosts/P16/nix.nix
@@ -19,7 +19,7 @@ boot.kernel.sysctl = {
    services.xserver = {
     enable = true;
     xkb.layout = "us";
-    videoDrivers = [ "nvidia" "intel" ];
+    videoDrivers = ["modesetting" "nvidia" "intel"];
     displayManager.sddm = {
       enable = true;
     };
